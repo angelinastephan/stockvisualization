5505be7ce5a4da8c80b2696ed82fa76c902d2538
angelina <angelina@yy-MacBook-Pro.hsd1.ca.comcast.net>
2019-10-02 20:19:47 -0700

    displaying the data in rows and columns

--- src/views/Base/Cards/Cards.js
+++ src/views/Base/Cards/Cards.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import { Badge, Card, CardBody, CardFooter, CardHeader, Col, Row, Collapse, Fade } from 'reactstrap';
-import { AppSwitch } from '@coreui/react'
+
 
 class Cards extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      stockData: []
+      stockData: [],
+      stockList: []
     };
   }
 
@@ -17,34 +18,39 @@ componentDidMount(){
   .then(response => response.json())
   .then(data => {
     this.setState({stockData:data});
-    this.buildMyCards();
+    this.buildStockList();
   })
 }
 
-  buildMyCards(){
-    console.log(this.state.stockData['Meta Data']);
-    console.log(this.state.stockData['Time Series (5min)']);
+buildStockList(){
+   // console.log(this.state.stockData['Meta Data']);
+   // console.log(this.state.stockData['Time Series (5min)']);
+   let items = Object.keys(this.state.stockData['Time Series (5min)']);
+   const sl = items.map((key, index)=>{
+     //console.log(this.state.stockData['Time Series (5min)'][key]);
+     //console.log(key);
+     return <div key={key}>
+     <Row>
+        <Col>
+        {JSON.stringify(key)}
+        </Col>
+        <Col>
+          {JSON.stringify(this.state.stockData['Time Series (5min)'][key])}
+        </Col>
+      </Row>
+    </div>
 
-  }
+   })
+
+   this.setState({stockList: sl});
+ }
 
   render() {
     return (
       <div className="animated fadeIn">
         <Row>
           <Col xs="12" sm="6" md="4">
-            <Card className="text-white bg-danger">
-              <CardHeader className= "text-white bg-info">
-                Card title
-              </CardHeader>
-              <CardBody>
-                Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut
-                laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation
-                ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
-              </CardBody>
-              <CardFooter className="text-black bg-info">
-              Contact Infomation
-              </CardFooter>
-            </Card>
+            {this.state.stockList}
           </Col>
           </Row>
        </div>
